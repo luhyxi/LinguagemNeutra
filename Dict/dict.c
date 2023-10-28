77c4fd40d9eebd6edfbbc32d885a47030da4bba4
luhyxi <luhyxi@gmail.com>
2023-10-28 10:38:34 -0300

    Edited whole dict logic and structure

--- Dict/dict.c
+++ Dict/dict.c
@@ -1,26 +1,27 @@
-#include "dict.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "dict.h"
 
-struct Data entryConstructor(const char *key, const char *val) {
-    struct Data entry;
-    entry.key = (char *)malloc(strlen(key) + 1);  // +1 for null terminator
-    entry.val = (char *)malloc(strlen(val) + 1);  // +1 for null terminator
+dictItem dictAddEntry(Dictionary* dict, const char *key, const char *val) {
+    // Allocate memory for a new item
+    dictItem newEntry;
+    newEntry.key = strdup(key);
+    newEntry.val = strdup(val);
 
-    if (entry.key && entry.val) {
-        strcpy(entry.key, key);
-        strcpy(entry.val, val);
-    } else {
-        // Handle memory allocation failure
-        // You can add appropriate error handling here
-    }
+    // Add the new item to the dictionary's entries
+    dict->entries = realloc(dict->entries, (dict->size + 1) * sizeof(dictItem));
+    dict->entries[dict->size] = newEntry;
+    dict->size++;
 
-    return entry;
-}
-void entryDestructor(struct Data *entry) {
-    free (entry->key);
-    free (entry->val);
-    free(entry);
+    return newEntry;
 }
 
+void freeItem(dictItem* entry) {
+    free(entry->key);
+    free(entry->val);
+}
+
+char* getVal(dictItem* entry) {
+    return entry->val;
+}
--- Dict/dict.h
+++ Dict/dict.h
@@ -1,16 +1,24 @@
-#ifndef LINGUAGEMNEUTRA_DICT_H
-#define LINGUAGEMNEUTRA_DICT_H
+#ifndef DICT_H
+#define DICT_H
 
-#define MAX_KEY_LENGTH 50
-#define MAX_VAL_LENGTH 50
+#include "stdlib.h"
 
-struct Data {
-    char *key[MAX_KEY_LENGTH];
-    char val[MAX_VAL_LENGTH];
+struct dictItem_ {
+    char* val;
+    char* key;
 };
 
-struct Data entryConstructor(const char *key, const char *val);
+typedef struct dictItem_ dictItem;
 
-void entryDestructor(struct Data *entry);
+typedef struct Dictionary {
+    dictItem* entries;
+    size_t size;
+} Dictionary;
 
-#endif // LINGUAGEMNEUTRA_DICT_H
+dictItem dictAddEntry(Dictionary* dict, const char *key, const char *val);
+
+void freeItem(dictItem* entry);
+
+char* getVal(dictItem* entry);
+
+#endif // DICT_H
